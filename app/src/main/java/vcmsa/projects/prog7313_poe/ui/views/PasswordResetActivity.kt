a106bf732aaf7ddc89f43bc9fb305b1d93817d33
ST10257002 <131689755+ST10257002@users.noreply.github.com>
2025-04-27 16:12:14 +0200

    feat(reset-act): implement view binding features to replace outdated view finding calls

--- app/src/main/java/vcmsa/projects/prog7313_poe/ui/views/PasswordResetActivity.kt
+++ app/src/main/java/vcmsa/projects/prog7313_poe/ui/views/PasswordResetActivity.kt
@@ -4,25 +4,28 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import vcmsa.projects.prog7313_poe.R
+import vcmsa.projects.prog7313_poe.databinding.ActivityPasswordResetBinding
 
 class PasswordResetActivity : AppCompatActivity() {
-    private lateinit var emailEditText: EditText
-    private lateinit var resetButton: Button
+    
+    private lateinit var binding: ActivityPasswordResetBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_password_reset)
-
-        emailEditText = findViewById(R.id.emailEditText)
-        resetButton = findViewById(R.id.resetButton)
+        
+        setupBindings()
+        
+        setContentView(binding.root)
+        
         auth = FirebaseAuth.getInstance()
 
-        resetButton.setOnClickListener {
-            val email = emailEditText.text.toString().trim()
+        binding.resetButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString().trim()
             if (email.isNotEmpty()) {
                 auth.sendPasswordResetEmail(email)
                     .addOnCompleteListener { task ->
@@ -37,4 +40,19 @@ class PasswordResetActivity : AppCompatActivity() {
             }
         }
     }
+    
+    
+    // <editor-fold desc="Configuration">
+
+
+    /**
+     * @author ST10257002
+     */
+    private fun setupBindings() {
+        binding = ActivityPasswordResetBinding.inflate(layoutInflater)
+    }
+    
+
+    // </editor-fold>
+    
 }
